cef70a533c07b49b53d1f835af87a68c91a397d5
Rodolfo Ochoa <rodolfo.ochoa@oracle.com>
2014-09-12 19:07:28 -0500

    Readme fixed

--- Readme.md
+++ Readme.md
@@ -1,19 +1,15 @@
-[![Build Status] (http://www.oracle.com/ocom/groups/public/@otn/documents/webcontent/1876686.jpg)](http://www.oracle.com/technetwork/database/database-technologies/nosqldb/overview/index.html)
+[(http://www.oracle.com/ocom/groups/public/@otn/documents/webcontent/1876686.jpg)](http://www.oracle.com/technetwork/database/database-technologies/nosqldb/overview/index.html)
 # KVClient API for node.js
 
-INTRODUCTION
-------------
+## INTRODUCTION
 The KVClient API for node.js provides a direct connection to any KVStore server. 
 
+## REQUIREMENTS
+KVClient uses Thrift (https://thrift.apache.org/) protocol to connect to any KVStore server. A running instance
+of a Thrift Proxy is required to establish a connection to a KVstore server. This package is capable of 
+starting an instance of this proxy by default, this Proxy requires you to have Java 7 runtime. 
 
-REQUIREMENTS
-------------
-KClient uses Thrift (https://thrift.apache.org/) protocol to connect to any KVStore server, in order to establish 
-a connection to a KVStore server, you must have a running instance of a Thrift Proxy. This package is capable of 
-starting an instance of this proxy by default, this Proxy requires you to have Java 7. 
-
-EXAMPLE
--------
+## EXAMPLE
 ```
 var kvClient = require('kvclient-js');
 var kvTypes = kvClient.Types;
@@ -36,20 +32,20 @@ store.open();
 
 ```
 
-DIRECTORY STRUCTURE
--------------------
+## DIRECTORY STRUCTURE
 <dl compact>
  <dt> documentation/   <dd>      API Reference
  <dt> examples/        <dd>      Sample code
  <dt> lib/             <dd>      The KVClient-js source code
  <dt> proxy/           <dd>      Required files to start a Thrift Proxy instance
- <dt> test/            <dd>      Test suite for mocha
+ <dt> test/            <dd>      Test cases for mocha
 </dl>
 
+## TESTING
+To test this package you will need to have on the database the test structure provided on:
+```test/db-config.ddl```
 
-TESTING
--------
-To run the test cases included in the package, you need to have mocha installed, to install mocha use:
+To run the test cases included in the package you also need to have mocha installed, to install mocha use:
 ```npm install mocha | sudo npm install -g mocha``` 
 
 then at package directory run the command:
@@ -57,12 +53,9 @@ then at package directory run the command:
 
 Mocha will automatically look for test directory and try to run the tests.
 
+## DOCUMENTATION
+The documentation is included in the documentation directory. 
 
-DOCUMENTATION
--------------
-For a reference of the package please go to documentation directory. 
-
-FOR MORE INFORMATION
---------------------
-See the issues and other information at http://github.com/oracle/kvclient-js/
+## FOR MORE INFORMATION
+For issues and more information, see http://github.com/oracle/kvclient-js/
 
--- test/put.js
+++ test/put.js
@@ -61,7 +61,7 @@ describe('KVClient', function(){
         function Row() {
             this.id = 100;       this.s = "String value";
             this.l = 1234567890; this.d = 1234567890.12345678901234567890;
-            this.bool = true;    this.bin = file64;
+            this.bool = true;    //this.bin = file64;
             this.fbin = fbin64;  this.f  = 1234567890.12345678901234567890;
             this.arrStr =  ["X", "Y", "Z"]; this.e = "A"; }
         var row = new Row();
@@ -70,108 +70,106 @@ describe('KVClient', function(){
             store.open(done);
         })
         this.timeout(100000); // debugging
-        describe('Put', function(){
-            it('Simple Put', function(done) {
-                var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
-                    kvTypes.ReplicaAckPolicy.ALL,
-                    kvTypes.SyncPolicy.SYNC);
-                var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
-                store.put('t2', row, writeOptions, done);
-            })
-            it('Put with default Durability', function(done) {
-                var writeOptions = new kvTypes.WriteOptions();
-                writeOptions.returnChoice = kvTypes.ReturnChoice.ALL;
-                writeOptions.timeout = 100;
-                store.put('t2', row, writeOptions, done);
 
-            })
-
-            it('empty Put', function(done) {
-                var writeOptions = new kvTypes.WriteOptions();
-                writeOptions.returnChoice = kvTypes.ReturnChoice.ALL;
-                writeOptions.timeout = 100;
-                var row =  null;
-                store.put('t2', row, writeOptions, function(err){
-                    done(assert.throws(err, function() {
-                            if ( (err instanceof Error) && err.message) {
-                                return true;
-                            }
-                        }
-                    ));
-                });
-            })
-/*
-            it('Put with all WriteOptions (108 combinations)', function(done) {
-                var durability, writeOptions;
-                var userid = 0, row, putCount=0;
-                var errorFlag = false;
-                for(var masterSync_KEY in kvTypes.ReplicaAckPolicy) {
-                    for(var replicaSync_KEY in kvTypes.ReplicaAckPolicy) {
-                        for (var replicaAck_KEY in kvTypes.SyncPolicy) {
-                            durability = kvTypes.newDurability(
-                                kvTypes.ReplicaAckPolicy[masterSync_KEY],
-                                kvTypes.ReplicaAckPolicy[replicaSync_KEY],
-                                kvTypes.SyncPolicy[replicaAck_KEY] );
-
-                            for (var ReturnChoice_KEY in kvTypes.ReturnChoice) {
-                                writeOptions = kvTypes.newWriteOptions(durability,
-                                    kvTypes.ReturnChoice[ReturnChoice_KEY],
-                                    1000)
-                                userid++;
-                                row =  {"uid":""+userid, "surname":"User"+userid};
-                                store.put('t2', row, writeOptions, function(err, response){
-                                    if (!errorFlag) {
-                                        if (err) {
-                                            errorFlag = true;
-                                            done(err);
-                                        }
-                                        ++putCount;
-                                        if ( putCount >= 108 ) {  // 108 Elements
-                                            done();
-                                        }
-                                    }
-                                });
-                            }
-                        }
-                    }
-                }
-            })
-            */
-        })
-/*
-        describe('PutIfAbsent', function(){
-            it('Simple putIfAbsent', function(done){
-                var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
-                    kvTypes.ReplicaAckPolicy.ALL,
-                    kvTypes.SyncPolicy.SYNC);
-                var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
-                store.putIfAbsent('t2', row, writeOptions, done);
-            })
-        })
-
-        describe('PutIfPresent', function(){
-            it('Simple putIfPresent', function(done){
-                var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
-                    kvTypes.ReplicaAckPolicy.ALL,
-                    kvTypes.SyncPolicy.SYNC);
-                var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
-                store.putIfPresent('t2', row, writeOptions, done);
-            })
-        })
+//        describe('Put', function(){
+//            it('Should do a simple Put', function(done) {
+//                var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
+//                    kvTypes.ReplicaAckPolicy.ALL,
+//                    kvTypes.SyncPolicy.SYNC);
+//                var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
+//                store.put('t2', row, writeOptions, done);
+//            })
+//            it('Should put with default Durability', function(done) {
+//                var writeOptions = new kvTypes.WriteOptions();
+//                writeOptions.returnChoice = kvTypes.ReturnChoice.ALL;
+//                writeOptions.timeout = 100;
+//                store.put('t2', row, writeOptions, done);
+//
+//            })
+//
+//            it('Will throw error with an empty Put', function(done) {
+//                var writeOptions = new kvTypes.WriteOptions();
+//                writeOptions.returnChoice = kvTypes.ReturnChoice.ALL;
+//                writeOptions.timeout = 100;
+//                var row =  null;
+//                store.put('t2', row, writeOptions, function(err){
+//                    done(assert.throws(err, function() {
+//                            if ( (err instanceof Error) && err.message) {
+//                                return true;
+//                            }
+//                        }
+//                    ));
+//                });
+//            })
+//
+//            it('Will Put with all WriteOptions (108 combinations)', function(done) {
+//                var durability, writeOptions;
+//                var putCount=0;
+//                var errorFlag = false;
+//                for(var masterSync_KEY in kvTypes.ReplicaAckPolicy) {
+//                    for(var replicaSync_KEY in kvTypes.ReplicaAckPolicy) {
+//                        for (var replicaAck_KEY in kvTypes.SyncPolicy) {
+//                            durability = kvTypes.newDurability(
+//                                kvTypes.ReplicaAckPolicy[masterSync_KEY],
+//                                kvTypes.ReplicaAckPolicy[replicaSync_KEY],
+//                                kvTypes.SyncPolicy[replicaAck_KEY] );
+//
+//                            for (var ReturnChoice_KEY in kvTypes.ReturnChoice) {
+//                                writeOptions = kvTypes.newWriteOptions(durability,
+//                                    kvTypes.ReturnChoice[ReturnChoice_KEY],
+//                                    1000)
+//                                row.id =  putCount;
+//                                store.put('t2', row, writeOptions, function(err, response){
+//                                    if (!errorFlag) {
+//                                        if (err) {
+//                                            errorFlag = true;
+//                                            done(err);
+//                                        }
+//                                        ++putCount;
+//                                        if ( putCount >= 108 ) {  // 108 Elements
+//                                            done();
+//                                        }
+//                                    }
+//                                });
+//                            }
+//                        }
+//                    }
+//                }
+//            })
+//
+//        })
+//
+//        describe('PutIfAbsent', function(){
+//            it('Simple putIfAbsent', function(done){
+//                var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
+//                    kvTypes.ReplicaAckPolicy.ALL,
+//                    kvTypes.SyncPolicy.SYNC);
+//                var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
+//                store.putIfAbsent('t2', row, writeOptions, done);
+//            })
+//        })
+//
+//        describe('PutIfPresent', function(){
+//            it('Simple putIfPresent', function(done){
+//                var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
+//                    kvTypes.ReplicaAckPolicy.ALL,
+//                    kvTypes.SyncPolicy.SYNC);
+//                var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
+//                store.putIfPresent('t2', row, writeOptions, done);
+//            })
+//        })
 
         describe('PutIfVersion', function(){
             it('Simple putIfVersion', function(done){
                 var readOptions = Types.newReadOptions(kvTypes.Consistency.ABSOLUTE);
-                var primaryKey = {"uid":"777"};
+                var primaryKey = {id:100};
                 store.get('t2', primaryKey, readOptions, function (err, response) {
-                    if (response) {
+                    if (typeof response !== "undefined") {
                         var durability = kvTypes.newDurability(kvTypes.ReplicaAckPolicy.ALL,
                             kvTypes.ReplicaAckPolicy.ALL,
                             kvTypes.SyncPolicy.SYNC);
                         var writeOptions = kvTypes.newWriteOptions(durability, kvTypes.ReturnChoice.ALL, 1000);
-                        store.putIfVersion('t2', row, response.version, writeOptions, function(err, response){
-                            done(err);
-                        });
+                        store.putIfVersion('t2', row, response.currentRowVersion, writeOptions, done);
                     } else {
                         done(err);
                     }
@@ -179,7 +177,7 @@ describe('KVClient', function(){
             })
 
         })
-*/
+
     })
 
 
